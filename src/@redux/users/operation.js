4906c3f34ae5d94e45289cb0c4dd6a5686afee33
mac <dimasobchenko.94@gmail.com>
2021-05-13 19:30:26 +0300

    new pack

--- src/@redux/users/operation.js
+++ src/@redux/users/operation.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {getUserSuccess, getRequest} from "./action";
 
-const URL_GET_USER = `http://176.105.100.114:7000/api/user/all  `;
+const URL_GET_USER = `http://176.105.100.114:7000/api/user/all `;
 
 export const getUser = () => (dispatch) => {
   dispatch(getRequest());
