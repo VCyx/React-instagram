17a505388a686b1df71e6e91d716f890bafa4a10
Vladyslav Suhyh <vcyx@ukr.net>
2021-05-16 14:35:38 +0300

    trying to fix responsive main page, menu button fixed

--- src/api/AxiosAPI.js
+++ src/api/AxiosAPI.js
@@ -7,26 +7,25 @@ export const mainURL = "http://176.105.100.114:7000/";
 export const avaURL = "http://176.105.100.114:7000/ava-user/";
 
 const host = axios.create(
-    {
-        baseURL: `http://${url}:${port}/`
-    },
-    []
+  {
+    baseURL: `http://${url}:${port}/`,
+  },
+  []
 );
 const authHost = axios.create(
-    {
-        baseURL: `http://${url}:${port}/`
-    },
-    []
+  {
+    baseURL: `http://${url}:${port}/`,
+  },
+  []
 );
 
-const verification = configuration => {
-    configuration.headers.authorization = `Bearer ${localStorage.getItem('token')}`
-    return configuration
-}
+const verification = (configuration) => {
+  configuration.headers.authorization = `Bearer ${localStorage.getItem(
+    "token"
+  )}`;
+  return configuration;
+};
 
-authHost.interceptors.request.use(verification)
+authHost.interceptors.request.use(verification);
 
-export {
-    host,
-    authHost
-}
+export { host, authHost };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Posts.module.scss";
 import PostItem from "../PostItem/PostItem";
 import Icon from "../Icon/Icon";
+import MenuButton from "../MenuButton/MenuButton";
 
 const Posts = ({ posts }) => {
   return (
@@ -9,8 +10,9 @@ const Posts = ({ posts }) => {
       <section className={styles.postsContainer}>
         <div className={styles.logo}>
           <Icon type="logo" />
-          {/*/!*<MenuButton blue className={styles.menuSmallScreen} />*!/ - що він робить?*/}
         </div>
+        <MenuButton blue className={styles.menuSmallScreen} />
+
         {posts &&
           posts.map((post) => {
             return (
